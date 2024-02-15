485206bacb7e2354607c8eecc3abf01071fa4b0c
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-02-15 12:35:41 -0300

    add payment-service

--- nestjs-api/src/orders/order.consumer.ts
+++ nestjs-api/src/orders/order.consumer.ts
@@ -18,9 +18,10 @@ export class OrderConsumer {
   @RabbitSubscribe({
     exchange: 'amq.direct',
     routingKey: 'PaymentDone',
-    queue: 'micro-orders',
+    queue: 'payments',
   })
   async consume(msg: { order_id: string; status: OrderStatus }) {
+    console.log('Message:', msg);
     try {
       if (msg.status === OrderStatus.PAID) {
         await this.orderService.pay(msg.order_id);
@@ -39,7 +40,6 @@ export class OrderConsumer {
         });
         return new Nack(false);
       }
-      //logica para contar quantas vezes uma mensagem foi consumida
       return new Nack(true);
     }
   }
---
+++ payment-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/guijoazeiro/imersaofullcycle
+
+go 1.21.6
+
+require github.com/rabbitmq/amqp091-go v1.9.0
